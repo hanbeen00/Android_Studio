0722d6f688915ebb17a912a323d2f54e14b4a6d6
hanbeen00 <76528601+hanbeen00@users.noreply.github.com>
2023-07-27 20:42:44 +0900

    Create MainActivity.java

---
+++ 프로젝트/간단한 날짜, 시간 예약/MainActivity.java
@@ -0,0 +1,83 @@
+package com.cookandroid.unicall3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.RadioButton;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Chronometer chrono;
+    Button btnStart, btnEnd;
+    RadioButton rBtnCalendar, rBtnTime;
+    CalendarView calView;
+    TimePicker tPicker;
+    int selectYear, selectMonth, selectDay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnEnd = (Button) findViewById(R.id.btnEnd);
+
+        chrono = (Chronometer) findViewById(R.id.chronometer);
+
+        rBtnCalendar = (RadioButton) findViewById(R.id.rBtnCalendar);
+        rBtnTime = (RadioButton) findViewById(R.id.rBtnTime);
+
+        tPicker = (TimePicker) findViewById(R.id.timePicker);
+        calView = (CalendarView) findViewById(R.id.calendarView);
+
+        tPicker.setVisibility(View.INVISIBLE);
+        calView.setVisibility(View.INVISIBLE);
+
+        rBtnCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tPicker.setVisibility(View.INVISIBLE);
+                calView.setVisibility(View.VISIBLE);
+            }
+        });
+
+        rBtnTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tPicker.setVisibility(View.VISIBLE);
+                calView.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chrono.setBase(SystemClock.elapsedRealtime());
+                chrono.start();
+                chrono.setTextColor(Color.RED);
+            }
+        });
+
+        btnEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chrono.stop();
+                chrono.setTextColor(Color.BLUE);
+                Toast.makeText(getApplicationContext(),
+                        Integer.toString(selectYear)+"년 " +
+                                Integer.toString(selectMonth)+"월 " +
+                                Integer.toString(selectDay)+"일 " +
+                                Integer.toString(tPicker.getCurrentHour())+"시 " +
+                                Integer.toString(tPicker.getCurrentMinute())+"분 저장됨"
+                        ,Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
